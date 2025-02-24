e799e021b00bd7f146e48d35ad2aed232197c7ed
Romain <romain.stefani@etu.unice.fr>
2025-02-24 08:46:00 +0100

    :boar: Mise en place architecture

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import './assets/css/style.css'
+import javascriptLogo from './assets/img/javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from './site/counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
