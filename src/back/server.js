6cca5eb1244ec79276b33d30ba27d97c166c5cb9
Romain <romain.stefani@etu.unice.fr>
2025-02-25 10:28:50 +0100

    :lady_beetle: Mise en place du projet, affichage dynamique des pages sans rechargement et server express

---
+++ src/back/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+app.use(cors()); 
+app.use(express.json());
+
+// Pour gérer correctement __dirname avec ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../front")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front/index.html"));
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
