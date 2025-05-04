031b35bd93a6932de7afbf000f93117c94fda134
clarator <116170772+clarator@users.noreply.github.com>
2025-05-04 12:03:21 +0200

    maj

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src/front/site',  // Définir la racine du projet
   build: {
-    outDir: 'dist',  // Indiquer où mettre les fichiers de build (par défaut dist)
-    rollupOptions: {
-      input: {
-        main: 'src/front/site/index.html'  // Spécifier explicitement le fichier d'entrée
-      }
-    }
+    outDir: '../../dist',  // Indiquer où mettre les fichiers de build (relative à 'root')
   }
 });
