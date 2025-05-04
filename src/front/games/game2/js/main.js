65d2c9adad7aee862534255c3a4d111445210bf3
Romain <romain.stefani@etu.unice.fr>
2025-05-04 18:11:35 +0200

    Game 2

--- src/front/games/game2/js/main.js
+++
@@ -1,8 +0,0 @@
-import Game from "./game.js";
-
-// Point d'entrée du jeu
-window.addEventListener('load', () => {
-    // Créer et démarrer le jeu
-    const game = new Game('gameCanvas');
-    game.start();
-});
