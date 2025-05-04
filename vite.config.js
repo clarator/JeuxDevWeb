aca8ab08ce8f9151fbc0dae2359edb71fb71621b
clarator <116170772+clarator@users.noreply.github.com>
2025-05-04 12:04:52 +0200

    maj2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ export default defineConfig({
   root: 'src/front/site',  // Définir la racine du projet
   build: {
     outDir: '../../dist',  // Indiquer où mettre les fichiers de build (relative à 'root')
-  }
+    emptyOutDir: true,
+}
 });
