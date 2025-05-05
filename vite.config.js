b5d4578274ee58f57dc6f04963eb393625eecfc5
clarator <116170772+clarator@users.noreply.github.com>
2025-05-05 22:37:45 +0200

    test pour les jeux

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true, 
+    rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'src/front/site/index.html'),
+          game1: path.resolve(__dirname, 'src/front/games/game1/game1.html'),
+          game2: path.resolve(__dirname, 'src/front/games/game2/game2.html'),
+          game3: path.resolve(__dirname, 'src/front/games/game3/game3.html'),
+        },
+      },
     },
    
 });
