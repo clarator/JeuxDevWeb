c09a6587180d4e9bf0c2b0b41b83eed59d63241c
clarator <116170772+clarator@users.noreply.github.com>
2025-05-05 21:53:46 +0200

    changement pour test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',      
+  root: 'src/site',      
   build: {
-    outDir: '../dist' 
+    outDir: '../../../dist' 
   }
 });
