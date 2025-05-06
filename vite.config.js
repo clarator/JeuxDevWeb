a44a7220efd0389b8fcecadf1639623ec59ccc7b
clarator <116170772+clarator@users.noreply.github.com>
2025-05-06 10:46:24 +0200

    changement

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 export default defineConfig({
   root: 'src/front/site',     
   publicDir: 'public', 
+  base: './',
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true, 
